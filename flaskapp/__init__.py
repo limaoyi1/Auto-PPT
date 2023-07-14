1644dd518fdbb9b93d2009f4c49813f0f76a6e2b
李茂益 <limaoyi@tianhecloud.com>
2023-07-14 17:04:43 +0800

    update app init

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,2 +1,4 @@
-import sys # 即添加包名的搜索路径
-sys.path.append("./../")
+import sys
+import os
+# 把当前文件所在文件夹的父文件夹路径加入到PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
