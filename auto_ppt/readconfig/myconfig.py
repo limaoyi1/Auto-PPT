2ed2b0146cf2be8f74f7beeb6f9be6d7fdbb85f5
李茂益 <limaoyi@tianhecloud.com>
2023-08-15 10:57:17 +0800

    修改读取配置的方式

--- auto_ppt/readconfig/myconfig.py
+++ auto_ppt/readconfig/myconfig.py
@@ -4,8 +4,10 @@ import os
 
 config = configparser.ConfigParser()
 # 指定编码为 UTF-8
+# 获取当前运行的Python文件的绝对路径
+current_file_path = os.path.abspath(__file__)
 
-base = "E:\\LocalWarehouse\\github\\Auto-PPT\\auto_ppt\\"
+base = current_file_path.replace("myconfig.py", "") + "../"
 
 
 class MyConfig:
@@ -20,7 +22,7 @@ class MyConfig:
     def __init__(self):
         current_path = os.getcwd()
         print("当前路径:", current_path)
-        config.read_file(codecs.open(base+'config.ini', 'r', 'utf-8-sig'))
+        config.read_file(codecs.open(base + 'config.ini', 'r', 'utf-8-sig'))
         self.Real_File = config.get('Credentials', 'Real_File')
         if self.Real_File == "Config.ini":
             self.OPENAI_BASE_URL = config.get('Credentials', 'OPENAI_BASE_URL')
@@ -30,7 +32,7 @@ class MyConfig:
             self.REDIS_ENABLE = config.get('Credentials', 'REDIS_ENABLE')
             self.REDIS_URL = config.get('Credentials', 'REDIS_URL')
         else:
-            config.read_file(codecs.open(base+self.Real_File, 'r', 'utf-8-sig'))
+            config.read_file(codecs.open(base + self.Real_File, 'r', 'utf-8-sig'))
             self.OPENAI_BASE_URL = config.get('Credentials', 'OPENAI_BASE_URL')
             self.OPENAI_API_KEY = config.get('Credentials', 'OPENAI_API_KEY')
             self.UNSPLASH_API_KEYS = config.get('Credentials', 'UNSPLASH_API_KEY').split(',')
@@ -38,6 +40,7 @@ class MyConfig:
             self.REDIS_ENABLE = config.get('Credentials', 'REDIS_ENABLE')
             self.REDIS_URL = config.get('Credentials', 'REDIS_URL')
         print(self.OPENAI_API_KEY)
+
 
 if __name__ == '__main__':
     my_config = MyConfig()
