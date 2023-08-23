05ee972caf25553485c510ec49deb3956f6c010c
李茂益 <limaoyi@tianhecloud.com>
2023-08-23 11:03:07 +0800

    添加 gunicorn_config.py配置

---
+++ auto_ppt/gunicorn.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/8/23 11:02
+# @Author  : limaoyi
+# @File    : gunicorn.py
+# @Software: PyCharm
+# @GitHub  : https://github.com/limaoyi1/GPT-prompt
+from application import app
